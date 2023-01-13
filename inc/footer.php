228447a5a01d0f41070f95e8fe1e10deda36372e
Yago Nieto Garrido <nietogarridoyago@gmail.com>
2023-01-13 10:51:04 +0100

    first commit

---
+++ inc/footer.php
@@ -0,0 +1,73 @@
+<footer class="negro">
+
+    <div class="logo">
+        <img src="../logos/image.png" class="imagenLogo">
+        <span>&nbsp;&nbsp;ROAD RUNNER</span>
+    </div>
+    <br><!-- Susutituir br por margin -->
+    <div></div>
+    <div id="primeraColumna">
+        <div id="RRSS">
+            <span class="rojo botonCirculoPequeño">
+                <img src="../Iconos/facebook.svg" alt="facebook" class="iconoPequeño">
+            </span>
+            <span class="rojo botonCirculoPequeño">
+                <img src="../Iconos/linkedin.svg" alt="linkedin" class="iconoPequeño">
+            </span>
+            <span class="rojo botonCirculoPequeño">
+                <img src="../Iconos/instagram.svg" alt="instagram" class="iconoPequeño">
+            </span>
+            <span class="rojo botonCirculoPequeño">
+                <img src="../Iconos/twitter.svg" alt="twitter" class="iconoPequeño">
+            </span>
+            <span class="rojo botonCirculoPequeño">
+                <img src="../Iconos/youtube.svg" alt="youtube" class="iconoPequeño">
+            </span>
+        </div>
+        <br>
+        <div id="usos">
+            <div class="boton rojo">
+                <img src="../Iconos/router.svg" alt="youtube" class="iconoPequeño">
+                <span>&nbsp;&nbsp;On-Line</span>
+            </div>
+            <div class="boton rojo">
+                <img src="../Iconos/apple.svg" alt="youtube" class="iconoPequeño">
+                <span>&nbsp;&nbsp;App store</span>
+            </div>
+            <div class="boton rojo">
+                <img src="../Iconos/google-play.svg" alt="youtube" class="iconoPequeño">
+                <span>&nbsp;&nbsp;Play Store</span>
+            </div>
+        </div>
+    </div>
+
+    <div id="segundaColumna">
+        <span class="tituloPequeño">EMPRESA</span>
+        <ul>
+            <li>Acerca de</li>
+            <li>Únete al equipo</li>
+            <li>Prensa</li>
+        </ul>
+    </div>
+    <div id="terceraColumna">
+        <span class="tituloPequeño">ENLACES ÚTILES</span>
+        <ul>
+            <li>Soporte técnico</li>
+            <li>Contacto</li>
+            <li>Road Runner +</li>
+            <li>Desarrolladores</li>
+        </ul>
+    </div>
+    <div id="cuartaColumna">
+        <span>Mapas</span> 
+        <span>Privacidad</span>
+        <span>Términos de uso</span> 
+    </div>
+    <div id="quintaColumna">
+        © 2023 Road Runner S.L.
+    </div>
+
+</footer>
+</body>
+
+</html>
---
+++ php/index.php
@@ -0,0 +1,166 @@
+<main>
+    <div id="fondoBusqueda">
+        <!-- <div id="busqueda">
+            <div class="azul" id="circulo"> <img src="../Iconos/search.svg" alt="" class="iconoPequeño"></div>
+            &nbsp;&nbsp;Encuentra tu próxima aventura 
+
+        </div> -->
+        <form action="busqueda.php" method="post">
+            <!-- <input type="image" src="../Iconos/search.svg" alt="" class="azul iconoPequeño" id="circulo"> -->
+            <!-- <div class="azul" id="circulo"> <img src="../Iconos/search.svg" alt="" class="iconoPequeño"></div> -->
+            <input type="submit" value="" class="azul " id="circulo">
+            <input type="image" src="../Iconos/search.svg" class="iconoPequeño" id="lupa">
+            <input id="busqueda" placeholder="Encuentra tu próxima aventura">
+        </form>
+    </div>
+
+    <section>
+        <h1>Rutas recomendadas</h1>
+        <div id="rutasRecomendadas">
+            <a class="link" href="detalleRuta.php">
+                <div id="rutaRecomendada">
+                    <img src="../img/pexels-rachel-claire-4997850.jpg" alt="" class="imagenPrincipal">
+                    <img src="../img/pexels-vanessa-garcia-6324457.jpg" alt="" class="imagenSegunda">
+                    <img src="../img/pexels-vanessa-garcia-6324238.jpg" alt="" class="imagenTercera">
+
+                    <div class="tituloPequeño titulo">Sendero de O Monte</div>
+                    <div class=" corazones"><img src="../Iconos/suit-heart-fill.svg" alt="" fill="#AB7C94" class="iconoPequeño">&nbsp;&nbsp;3214</div>
+                    <div class="caract1">
+                        <div>Distancia: 5km</div>
+                        <div>Intensidad: Media</div>
+                    </div>
+                    <div class="caract2">
+                        <div>Tipo: Circular</div>
+                        <div>Desnivel: 100m</div>
+                    </div>
+                </div>
+            </a>
+            <a class="link" href="detalleRuta.php">
+                <div id="rutaRecomendada" onclick="">
+
+                    <img src="../img/pexels-rachel-claire-4997850.jpg" alt="" class="imagenPrincipal">
+                    <img src="../img/pexels-vanessa-garcia-6324457.jpg" alt="" class="imagenSegunda">
+                    <img src="../img/pexels-vanessa-garcia-6324238.jpg" alt="" class="imagenTercera">
+
+                    <div class="tituloPequeño titulo">Sendero de O Monte</div>
+                    <div class=" corazones"><img src="../Iconos/suit-heart-fill.svg" alt="" fill="#AB7C94" class="iconoPequeño">&nbsp;&nbsp;3214</div>
+
+                    <div class="caract1">
+                        <div>Distancia: 5km</div>
+                        <div>Intensidad: Media</div>
+                    </div>
+                    <div class="caract2">
+                        <div>Tipo: Circular</div>
+                        <div>Desnivel: 100m</div>
+                    </div>
+                </div>
+            </a>
+            <a class="link" href="detalleRuta.php">
+                <div id="rutaRecomendada" onclick="">
+
+                    <img src="../img/pexels-rachel-claire-4997850.jpg" alt="" class="imagenPrincipal">
+                    <img src="../img/pexels-vanessa-garcia-6324457.jpg" alt="" class="imagenSegunda">
+                    <img src="../img/pexels-vanessa-garcia-6324238.jpg" alt="" class="imagenTercera">
+
+                    <div class="tituloPequeño titulo">Sendero de O Monte</div>
+                    <div class=" corazones"><img src="../Iconos/suit-heart-fill.svg" alt="" fill="#AB7C94" class="iconoPequeño">&nbsp;&nbsp;3214</div>
+
+                    <div class="caract1">
+                        <div>Distancia: 5km</div>
+                        <div>Intensidad: Media</div>
+                    </div>
+                    <div class="caract2">
+                        <div>Tipo: Circular</div>
+                        <div>Desnivel: 100m</div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </section>
+
+    <section style="padding-bottom: 40px;">
+        <h1>¿Qué te apetece hacer?</h1>
+        <div id="actividades">
+            <div class="botonCirculoPequeño rojo"><img src="../Iconos/caret-left.svg" alt="" class="iconoPequeño"></div>
+            <a class="link" href="busqueda.php">
+                <div class="actividad">
+                    <img src="../img/senderismo.jpg" alt="">
+                    <div class="textoSombra">Senderismo</div>
+                </div>
+            </a>
+            <a class="link" href="busqueda.php">
+                <div class="actividad">
+                    <img src="../img/montanismo.jpg" alt="">
+                    <div class="textoSombra">Montañismo</div>
+                </div>
+            </a>
+            <a class="link" href="busqueda.php">
+                <div class="actividad">
+                    <img src="../img/correr.jpg" alt="">
+                    <div class="textoSombra">Correr</div>
+                </div>
+            </a>
+            <a class="link" href="busqueda.php">
+                <div class="actividad">
+                    <img src="../img/ciclismo.jpg" alt="">
+                    <div class="textoSombra">Ciclismo</div>
+                </div>
+            </a>
+            <div class="botonCirculoPequeño rojo"><img src="../Iconos/caret-right.svg" alt="" class="iconoPequeño"></div>
+        </div>
+    </section>
+
+    <section id="venta1" class="azul">
+        <img src="../img/personasMovil.jpg" alt="" class="imgGrande">
+        <span class="spanDeSpans">
+            <span class="tituloPequeño">Rutas en la palma de tu mano</span><br><br>
+            <span class="texto">Todo lo que necesitas para hacer tus rutas favoritas y descubrir otras nuevas lo tienes aquí</span>
+        </span>
+    </section>
+
+    <section id="venta2" class="azul">
+        <img src="../img/barbas.jpg" alt="" class="imgGrande">
+        <span class="spanDeSpans">
+            <span class="tituloPequeño">Únete para disfrutar al aire libre sin <br> distrac<wbr>ciones</span><br><br>
+            <br><br>
+            <div>
+                <div class="boton rojo">
+                    <img src="../Iconos/router.svg" alt="youtube" class="iconoPequeño">
+                    <span>&nbsp;&nbsp;On-Line</span>
+                </div>
+                <br>
+                <div class="boton rojo">
+                    <img src="../Iconos/apple.svg" alt="youtube" class="iconoPequeño">
+                    <span>&nbsp;&nbsp;App store</span>
+                </div>
+                <br>
+                <div class="boton rojo">
+                    <img src="../Iconos/google-play.svg" alt="youtube" class="iconoPequeño">
+                    <span>&nbsp;&nbsp;Play Store</span>
+                </div>
+            </div>
+        </span>
+    </section>
+
+    <section id="caracteristicas">
+        <div>
+            <img src="../Iconos/fingerprint.svg" alt="" class="rojo">
+            <h1>Registrate</h1>
+            <p>Registra fácilmente tu rutas y compártelas con la comunidad</p>
+        </div>
+        <div>
+            <img src="../Iconos/plus-lg.svg" alt="" class="rojo">
+            <h1>Crea</h1>
+            <p>Convierte tus actividades en historias memorables</p>
+        </div>
+        <div>
+            <img src="../Iconos/suit-heart-fill-black.svg" alt="" class="rojo">
+            <h1>Conecta</h1>
+            <p>Conoce nuevas rutas y compártelas con la comunidad</p>
+        </div>
+    </section>
+
+    <section id="comentariosFalsos">
+
+    </section>
+</main>
