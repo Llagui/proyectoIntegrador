bf26de028945abf2fa83dc75801001d090b2b950
Yago Nieto Garrido <nietogarridoyago@gmail.com>
2023-01-15 20:29:58 +0100

    Pagina inicio sesion casi terminada

--- php/sesion.php
+++ php/sesion.php
@@ -1,11 +1,21 @@
-<main>
-    <center>
-        <br>
-        <br>
-        <h1>Pagina sesion</h1>
-        <h2>Estoy trabajado en ello</h2>
-        <img src="../img/computer-drinking.gif" id="trabajando">
-        <br>
-        <a href="index.php">Volver a landing page</a>
-    </center>
+<main id="contenedor">
+    <img src="../img/pexels-suliman-sallehi-1822461.jpg" alt="" class="imgFondoIzquierda">
+
+    <div class="azul" id="formularioSesion">
+        <h1>¡Hola de nuevo! <br> Inicia sesión y empieza a explorar</h1>
+        <form action="." method="post">
+            <label for="correoOUsuario">Correo electrónico o Usuario</label><br>
+            <input type="text" name="correoOUsuario" id="correoOUsuario" class="campo"><br><br>
+            <label for="contraseña">Contraseña</label><br>
+            <input type="password" name="contraseña" id="contraseña" class="campo"><br><br>
+            <center>
+                <input type="submit" value="Iniciar Sesión" class="boton rojo">
+                <p>
+                    ¿No tienes cuenta? <br>
+                    <a href="registro.php">Registrate aquí</a>
+                </p>
+            </center>
+        </form>
+    </div>
+    <img src="../img/pexels-lan-yao-13103876.jpg" alt="" class="imgFondoDerecha">
 </main>
